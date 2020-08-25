254018619d9f7dd17e3859ba907abc21277ecb4b
Frank Fenton <echelon.iiii@gmail.com>
2020-08-25 16:51:04 +1000

    fix: remove node config that was broken and uneeded

--- node.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: ['node'],
-  extends: [
-    'plugin:node/recommended', // node js has a few rules about using it's libraries that we should follow
-  ],
-  rules: {},
-};
