00591bf242d366a0b234ce1ad0a4d51d0c2cd6e0
Frank Fenton <echelon.iiii@gmail.com>
2022-01-13 23:45:09 +1100

    fix: fix the react config to work on lower node versions

--- react.js
+++ react.js
@@ -1,6 +1,15 @@
-const base = require('./index');
-const reactConfig = structuredClone(base);
+const base = require('./index.js');
+
+function deepClone(objectToClone) {
+  if (typeof structuredClone === 'function') {
+    return structuredClone(objectToClone); // only comes in on node 17, but is better
+  }
+  return JSON.parse(JSON.stringify(objectToClone));
+}
+
+const reactConfig = deepClone(base); // deep clone the base config, as we modify it to extend it
 reactConfig.extends = base.extends.flatMap((config) => {
+  // change the base configs to their react counterparts
   if (config === 'airbnb-base') {
     return ['airbnb', 'airbnb/hooks'];
   }
@@ -9,4 +18,5 @@ reactConfig.extends = base.extends.flatMap((config) => {
   }
   return config;
 });
-return reactConfig;
+
+module.exports = reactConfig;
