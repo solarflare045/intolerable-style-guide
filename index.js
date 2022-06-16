21d4fbede02bdd47d9b8a2e3aa40dc0c15579e14
Max Sorensen <max.sorensen@fusion5.com.au>
2022-06-16 16:08:04 +1000

    fix: :bug: disable additional linting rules in spec/cy files

--- index.js
+++ index.js
@@ -246,16 +246,18 @@ module.exports = {
       },
     },
     {
-      files: ['**/*.test.*', '**/*.spec.*'],
+      files: ['**/*.test.*', '**/*.spec.*', '**/*.cy.*'],
       rules: {
         'sonarjs/no-duplicate-string': 'off', // we duplicate strings in tests all the time and they shouldn't be the same instance in tests for isolation
         'sonarjs/no-identical-functions': 'off', // tests are often bad bad on purpose
         '@typescript-eslint/no-magic-numbers': 'off', // magic number are fun and good in tests
         // tests are full of any casts and whatnot
-        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
       },
     },
   ],
